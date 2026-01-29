c235e55f8a65dbf612baa4597c6c7648e34f5f5b
Riyank Mukhopadhyay <148816249+rmukhop3@users.noreply.github.com>
2026-01-29 14:43:03 -0700

    chore: alt-text for No match to Course not Found and Exact Match to Suggested Matches

--- content.js
+++ content.js
@@ -516,7 +516,7 @@ function generatePopupHTML(courseData, response) {
     switch (type) {
       case 'exact':
         icon = '<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>';
-        label = 'Exact Match';
+        label = 'Suggested Matches';
         className = 'match-status--exact';
         break;
       case 'strong_fuzzy':
@@ -527,7 +527,7 @@ function generatePopupHTML(courseData, response) {
         break;
       case 'no_match':
         icon = '<svg viewBox="0 0 24 24"><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"/></svg>';
-        label = 'No Match';
+        label = 'Course Not Found';
         className = 'match-status--none';
         break;
       case 'catalog_not_found':
